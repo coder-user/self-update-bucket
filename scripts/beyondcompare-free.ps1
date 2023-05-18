78c0a0ce8dbdb260b7692cf18ccc9c31a422613f
coder-user <35922343+coder-user@users.noreply.github.com>
2023-05-18 16:14:19 +0800

    Update beyondcompare-free.ps1

--- scripts/beyondcompare-free.ps1
+++ scripts/beyondcompare-free.ps1
@@ -2,7 +2,7 @@ $Script = {
     Remove-ItemProperty Registry::'HKEY_CURRENT_USER\Software\Scooter Software\Beyond Compare 4' 'CacheID' -ErrorAction Ignore
 }
 $Action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-NonInteractive -NoLogo -NoProfile -WindowStyle Hidden -Command $Script"
-$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Days 7)
+$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 6)
 $Principal = New-ScheduledTaskPrincipal -UserID "$env:USERNAME" -LogonType S4U
 Register-ScheduledTask -TaskName 'reset-beyondcompare-trial' -Action $Action -Trigger $Trigger -Principal $Principal -Force
 Start-ScheduledTask -TaskName 'reset-beyondcompare-trial'
